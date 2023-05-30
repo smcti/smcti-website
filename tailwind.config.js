36466b0d5e15eb5f07c3dcfd75685008651cb06b
satilpereira <satil.prog@gmail.com>
2023-05-30 20:31:42 -0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,6 +18,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'cello': {
+          DEFAULT: '#1E3560',
+          50: '#C4D2EC',
+          100: '#B4C6E8',
+          200: '#95AEDE',
+          300: '#7697D4',
+          400: '#577FCA',
+          500: '#3B68BD',
+          600: '#31579E',
+          700: '#28467F',
+          800: '#1E3560',
+          900: '#111D35',
+          950: '#0A1220'
+        },
+      }
     },
   },
   plugins: [],
