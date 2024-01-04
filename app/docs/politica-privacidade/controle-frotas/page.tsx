4738c02cce2889fce1d95cea3cc149c7958de5c3
Felipe Cattani <lipecattani@gmail.com>
2024-01-04 16:25:09 -0300

    build(Insert Docs page): Insert Politica de Privacidade - Controle de Frotas

---
+++ app/docs/politica-privacidade/controle-frotas/page.tsx
@@ -0,0 +1,75 @@
+
+const page = () => {
+    return (
+        <section>
+        <div className="section-default my-32 flex flex-col gap-8 text-gray-700">
+            <h1 className="text-xl md:text-3xl font-bold">
+                Política de Privacidade para Aplicativo de Gestão de Frotas
+            </h1>
+            <p>
+                Introdução:
+            </p>
+            <p>
+                Bem-vindo ao nosso aplicativo de gestão de frotas. Agradecemos por escolher nossos serviços para otimizar e aprimorar a gestão de sua frota. Esta política de privacidade foi desenvolvida para garantir a transparência, segurança e confiança em relação ao uso de suas informações pessoais.
+            </p>
+            <p>
+                Coleta de Informações:
+            </p>
+            <p>
+                Localização em Tempo Real e segundo plano: Para proporcionar uma experiência eficiente, nosso aplicativo pode coletar e processar dados de localização em tempo real e em segundo plano. Essas informações são essenciais para monitorar e aprimorar a eficiência da sua frota. Ressaltamos que tais dados não serão compartilhados com terceiros e serão utilizados exclusivamente para os propósitos de gestão de frota.
+            </p>
+            <p>
+                E-mail e Senha do Usuário: Ao criar uma conta em nosso aplicativo, solicitamos seu endereço de e-mail e uma senha segura. Estas informações são necessárias para autenticar sua identidade e proteger o acesso não autorizado à sua conta. Garantimos que suas credenciais de login não serão compartilhadas, vendidas ou disponibilizadas a terceiros.
+            </p>
+            <p>
+                Uso das Informações:
+            </p>
+            <p>
+                Melhoria do Serviço: As informações de localização são utilizadas para melhorar a eficiência operacional da sua frota, identificar áreas de melhoria e oferecer soluções personalizadas.
+            </p>
+            <p>
+                Comunicações: Podemos utilizar seu endereço de e-mail para comunicações importantes relacionadas ao serviço, como atualizações, manutenções ou notificações importantes sobre sua conta.
+            </p>
+            <p>
+                Segurança: Suas informações são tratadas com o mais alto nível de segurança. Implementamos medidas técnicas e organizacionais para proteger seus dados contra acesso não autorizado, alteração, divulgação ou destruição não autorizada.
+            </p>
+            <p>
+                Compartilhamento de Informações:
+            </p>
+            <p>
+                Garantimos que suas informações pessoais não serão compartilhadas com terceiros, exceto nos seguintes casos:
+            </p>
+            <p>
+                Quando necessário para cumprir obrigações legais.
+            </p>
+            <p>
+                Para proteger e defender nossos direitos legais.
+            </p>
+            <p>
+                Em casos de fusão, aquisição ou venda de ativos, onde suas informações podem ser transferidas como parte do processo.
+            </p>
+            <p>
+                Armazenamento e Retenção de Dados:
+            </p>
+            <p>
+                Suas informações serão armazenadas em servidores seguros. Retemos seus dados apenas pelo tempo necessário para cumprir os propósitos descritos nesta política ou conforme exigido por leis aplicáveis.
+            </p>
+            <p>
+                Atualizações da Política de Privacidade:
+            </p>
+            <p>
+                Reservamo-nos o direito de atualizar esta política de privacidade periodicamente. Notificaremos sobre quaisquer alterações significativas, e recomendamos que você reveja esta política regularmente.
+            </p>
+            <p>
+                Ao continuar a usar nosso aplicativo, você concorda com os termos desta política de privacidade. Se tiver dúvidas ou preocupações, entre em contato conosco através dos canais de suporte disponíveis no aplicativo.
+            </p>
+            <p>
+                Obrigado por confiar em nós para a gestão eficiente de sua frota!
+            </p>
+
+        </div>
+        </section>
+    )
+}
+
+export default page
