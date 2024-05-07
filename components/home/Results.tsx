adb42a56630ddd3bceea13fc1b1874c17495f798
Nakagawadesu <matheus@Matheus.matheus>
2024-05-07 10:26:18 -0300

    🐛 - fix:  Fix formatting

--- components/home/Results.tsx
+++ components/home/Results.tsx
@@ -1,4 +1,4 @@
-import resultsJson from '@public/data/resultsStartups.json'
+import resultsJson from "@public/data/resultsStartups.json";
 
 interface ResultsData {
   [key: number]: {
@@ -14,42 +14,60 @@ interface ResultsData {
 
 const results: ResultsData = resultsJson;
 
-
 const Results = () => {
   let ano = 2022;
 
   return (
     <section className="bg-cello text-zircon-50 py-32">
       <div className="section-default flex flex-col gap-8 lg:gap-16 items-center">
-        <h2 className='font-black text-2xl
+        <h2
+          className="font-black text-2xl
           sm:text-3xl
-          lg:text-4xl'>Resultados { ano }</h2>
-        <h3 className="font-black text-3xl text-cyan-400
+          lg:text-4xl"
+        >
+          Resultados {ano}
+        </h2>
+        <h3
+          className="font-black text-3xl text-cyan-400
           sm:text-5xl
-          lg:text-6xl">R$ { results[ano]?.valor }</h3>
-        <p className='lg:text-2xl sm:text-xl text-lg text-center font-semibold'>Em faturamento das empresas âncoras e incubadas no parque</p>
-        <div className="flex flex-col gap-8 w-full font-black 
-        lg:flex-row lg:justify-between">
-          {
-            Object.values(results[ano]?.arrecadamentos).map((item, index) => {
-              return (
-                <>
-                  <div className="flex flex-col gap-2 items-center">
-                    <h4 className='text-3xl text-cyan-400
+          lg:text-6xl"
+        >
+          R$ {results[ano]?.valor}
+        </h3>
+        <p className="lg:text-2xl sm:text-xl text-lg text-center font-semibold">
+          Em faturamento das empresas âncoras e incubadas no parque
+        </p>
+        <div
+          className="flex flex-col gap-8 w-full font-black 
+        lg:flex-row lg:justify-between"
+        >
+          {Object.values(results[ano]?.arrecadamentos).map((item, index) => {
+            return (
+              <>
+                <div className="flex flex-col gap-2 items-center">
+                  <h4
+                    className="text-3xl text-cyan-400
                       sm:text-4xl
-                      lg:text-5xl'>{item}</h4>
-                    <p>{Object.keys(results[ano]?.arrecadamentos)[index] != 'empregos' ?
-                      `Arrecadação ${Object.keys(results[ano]?.arrecadamentos)[index]}` :
-                      'Empregos registrados'}</p>
-                  </div>
-                </>
-              )
-            })
-          }
+                      lg:text-5xl"
+                  >
+                    {item}
+                  </h4>
+                  <p>
+                    {Object.keys(results[ano]?.arrecadamentos)[index] !=
+                    "empregos"
+                      ? `Arrecadação ${
+                          Object.keys(results[ano]?.arrecadamentos)[index]
+                        }`
+                      : "Empregos registrados"}
+                  </p>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
